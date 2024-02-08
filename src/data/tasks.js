364723c344b8ad5db542a382cf440443d8ef6198
Zewuck <deyner.higuera.26@gmail.com>
2024-02-08 10:17:15 -0500

    Aplicacion de react con vite (curso de fatz) q with '#' will be ignored, and an empty message aborts the commit. e

---
+++ src/data/tasks.js
@@ -0,0 +1,27 @@
+export const tasks = [
+    {
+        id: 0,
+        title: "mi primer tarea",
+        description: "Mi primer tarea",
+    },{
+        id: 1,
+        title: "mi segunda tarea",
+        description: "Mi segunda tarea",
+    },{
+        id: 2,
+        title: "mi tercer tarea",
+        description: "Mi tercer tarea",
+    },{
+        id: 3,
+        title: "mi cuarta tarea",
+        description: "Mi cuarta tarea",
+    },{
+        id: 4,
+        title: "mi quinta tarea",
+        description: "Mi quinta tarea",
+    },{
+        id: 5,
+        title: "mi secta tarea",
+        description: "Mi secta tarea",
+    },
+];
